77d6659ecfc5d1471ee8323786c8a93578200539
Farah Yeazneen <farahyeazneen@Farahs-Air.home>
2022-05-03 19:57:49 -0400

    data.js is done

---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+const data = {
+  products: [
+    {
+      name: 'Automatic Pet Feedr',
+      slung: 'automatic-pet-feeder',
+      category: 'accessory',
+      image: '/images/Automatic pet feeder.jpg',
+      price: 30,
+      stockCount: 10,
+    },
+    {
+      name: 'Gucci Dog Harness',
+      slung: 'gucci-dog-harnes',
+      category: 'accessory',
+      image: '/images/Gucci-padded-dog-harness-2.jpg',
+      price: 25,
+      stockCount: 10,
+    },
+    {
+      name: 'Dog Blanket',
+      slung: 'leaf-shaped blanket',
+      category: 'accessory',
+      image: '/images/Leaf shaped dog blanket.jpg',
+      price: 40,
+      stockCount: 10,
+    },
+    {
+      name: 'Pet Play-Pen',
+      slung: 'pet-play-pen',
+      category: 'accessory',
+      image: '/images/pet play-pen.jpg',
+      price: 350,
+      stockCount: 10,
+    },
+    {
+      name: 'Recovery belt',
+      slung: 'recovery-belt',
+      category: 'accessory',
+      image: '/images/recovery belt.jpg',
+      price: 50,
+      stockCount: 10,
+    },
+  ],
+};
